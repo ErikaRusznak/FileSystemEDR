8c2646ed52b356e5327514ba99e8ec353960cfec
Erika Rusznak <erika.rusznak@student.upt.ro>
2022-05-05 15:27:20 +0300

    added

---
+++ event/main.py
@@ -0,0 +1,30 @@
+from event_model import Event
+from file.test import TestDatabase
+from response_model import Response, BaseResponse
+import uvicorn
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {'title': 'Json System'}
+
+
+@app.post("/events", response_model=Response)
+async def add_event(event: Event) -> Response:
+
+    db = TestDatabase()
+    risk = await db.find_data(some_key=event.file.file_hash)
+    r = -1
+    if risk is not None:
+        r = risk['risk_level']
+
+    return Response(file=BaseResponse(hash=event.file.file_hash, risk_level=r),
+                    process=BaseResponse(hash=event.last_access.hash, risk_level=r))
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
---
+++ file/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, UploadFile
+import aiohttp
+import uvicorn
+from test import TestDatabase
+app = FastAPI()
+
+@app.post("/scan-file")
+async def add_file(file: UploadFile):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                'https://beta.nimbus.bitdefender.net:443/liga-ac-labs-cloud/blackbox-scanner/',
+                data={'file': await file.read()}) as resp:
+            res = await resp.json()
+            db = TestDatabase()
+            await db.insert_data(res)
+            return res
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8001)
